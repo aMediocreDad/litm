01f45cf67a7f5313b4cdc807d5175e6aaaf2f012
aMediocreDad <amediocredad@outlook.com>
2024-07-23 16:31:16 +0200

    Release v23

--- version.sh
+++ version.sh
@@ -1,5 +1,6 @@
 VERSION=$(jq '.version + 1' system.json)
 jq --tab --arg version "$VERSION" '.version = ($version | tonumber) | .download |= gsub("v[0-9]+"; "v" + $version)' system.json > temp.json
 mv temp.json system.json
+npx -y @biomejs/biome check . --write
 git commit -am "Release v$VERSION"
-git tag "v$VERSION"
+git tag "v$VERSION" -m "v$VERSION"
